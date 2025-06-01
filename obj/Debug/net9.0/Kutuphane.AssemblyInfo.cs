a48538361d91ed9478ab1a18606d2a1a2c580d8e
ozlemm0 <ozlemdemirr@example.com>
2025-06-01 19:48:00 +0300

    tasarım

--- obj/Debug/net9.0/Kutuphane.AssemblyInfo.cs
+++ obj/Debug/net9.0/Kutuphane.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kutuphane")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f6b26defa6fad5fac2b0ee3b2d299212bd52feb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e51bb86816ee91c13c3f2d499e81062ff9d6b58f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kutuphane")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kutuphane")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
